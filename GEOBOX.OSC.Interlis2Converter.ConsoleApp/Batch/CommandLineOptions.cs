156343ff4517970c22c1c83ae9aeea1adffb6a69
Urs Sommerhalder <urs.sommerhalder@geobox.ch>
2025-08-22 11:45:41 +0200

    26.1 - Fix in Help Text

--- GEOBOX.OSC.Interlis2Converter.ConsoleApp/Batch/CommandLineOptions.cs
+++ GEOBOX.OSC.Interlis2Converter.ConsoleApp/Batch/CommandLineOptions.cs
@@ -22,19 +22,19 @@ namespace GEOBOX.OSC.Interlis2Converter.ConsoleApp.Batch
         /// <summary>
         /// Name of the file without file extension
         /// </summary>
-        [Option('o', "outputFile", HelpText = "Angabe des Datei-Namens (inkl. Dateiendung) für das Zusammengeführte Interlis (XTF-Datei).")]
+        [Option('o', "outputFile", HelpText = "Angabe des Datei-Namens (inkl. Dateiendung) für das Resultat, in der Regel eine Interlis-Datei (XTF-Datei).")]
         public string OutputFile { get; set; }
         
         /// <summary>
         /// Name of the folder
         /// </summary>
-        [Option('d', "outputDir", HelpText = "Angabe des Datei-Pfades (Ordner) für das Zusammengeführte Interlis (XTF-Datei).", Required=true)]
+        [Option('d', "outputDir", HelpText = "Angabe des Datei-Pfades (Ordner) für das Speicher des Resultates (Zusammengeführte Interlis (XTF-Datei) oder XTF-Dateien aus dem Download).", Required=true)]
         public string OutputDir { get; set; }
         
         /// <summary>
         /// Download config file name and path
         /// </summary>
-        [Option('c', "downloadConfig", HelpText = "Angabe zur Konfigurationsdatei-Pfades für den Service-Daten-Download.")]
+        [Option('c', "downloadConfig", HelpText = "Angabe des Pfades und Dateiname zur Konfigurationsdatei für den Download der Daten aus den Web-Services.")]
         public string DownloadConfig { get; set; }
 
         /// <summary>
